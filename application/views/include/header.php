d37e89952656213d1077af13aa325522dafbdd47
Steven Lu <tacticalazn@gmail.com>
2012-01-21 14:44:46 -0500

    syntax fixes

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -11,7 +11,7 @@
 
    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
    <script type="text/javascript" src="http://bxslider.com/sites/default/files/jquery.bxSlider.min.js"></script>
-   <script type="text/javascript" src="<?= base_url('resources/js/jquery.fancybox-1.3.4.pack.js') ?></script>
+   <script type="text/javascript" src="<?= base_url('resources/js/jquery.fancybox-1.3.4.pack.js') ?>"></script>
 
    <link href="<?= base_url('resources/css/style.css') ?>" rel="stylesheet" type="text/css" charset="utf-8" />
 </head>
