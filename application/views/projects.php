606d2839913f7fed41e4170503e516de06a07d1a
Steven Lu <tacticalazn@gmail.com>
2012-01-21 15:00:42 -0500

    Don't auto rotate or show stuff if less than 4 photos

--- application/views/projects.php
+++ application/views/projects.php
@@ -7,7 +7,7 @@
    <script type="text/javascript">
       $(document).ready(function(){
          var slider = $('#photos_container').bxSlider({
-            auto: true,
+            <? if (count($photos) > 3): ?>auto: true,<? else: ?>auto: false,<? endif; ?>
             controls: false
          });
       
@@ -27,7 +27,7 @@
 
    <?php if (count($photos) != 0): ?>
       <table border="0" cellpadding="0" cellspacing="0"><tr>
-         <td><div id="slider_control_left"></div></td>
+         <? if (count($photos) > 3): ?><td><div id="slider_control_left"></div></td><? endif; ?>
          <td><div id="photos_container">
             <?php $c = 0; foreach($photos as $photo): ?>
                <?php if (($c%3) == 0): ?>
@@ -41,7 +41,7 @@
                <?php endif; $c++; ?>
             <?php endforeach; ?>
          </div></td>
-         <td><div id="slider_control_right"></div></td>
+         <? if (count($photos) > 3): ?><td><div id="slider_control_right"></div></td><? endif; ?>
       </tr></table>
    <?php endif; ?>
 </div>
