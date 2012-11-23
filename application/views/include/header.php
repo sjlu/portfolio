c0da547461b28ea99e29167312ca664f8ecd28f0
Steven Lu <tacticalazn@gmail.com>
2012-11-23 12:23:18 -0500

    JS to the bottom.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,11 +8,7 @@
    <meta name="keywords" content="<?= $meta['meta_keywords'] ?>" />
 
    <title><? if(!empty($title)) echo $title.' - '; ?><?= $meta['title'] ?></title>
-
-   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-   <script type="text/javascript" src="http://bxslider.com/sites/default/files/jquery.bxSlider.min.js"></script>
-   <script type="text/javascript" src="<?= base_url('resources/js/jquery.fancybox-1.3.4.pack.js') ?>"></script>
-
+t
    <link href="<?= base_url('resources/css/style.css') ?>" rel="stylesheet" type="text/css" charset="utf-8" />
    <link href="<?= base_url('resources/css/jquery.fancybox-1.3.4.css') ?>" rel="stylesheet" type="text/css" charset="utf-8" />
 </head>
