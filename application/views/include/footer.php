8d070e840469bac1f7fa11736c253d93c26fc4b8
Steven Lu <tacticalazn@gmail.com>
2012-11-25 09:19:46 -0500

    Adding footer and bxslider.

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -6,5 +6,5 @@
 </body>
 <?= get_analytics(); ?>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://bxslider.com/sites/default/files/jquery.bxSlider.min.js"></script>
+<script type="text/javascript" src="<?= base_url('resources/js/jquery.bxslider.min.js') ?>"></script>
 <script type="text/javascript" src="<?= base_url('resources/js/jquery.fancybox-1.3.4.pack.js') ?>"></script>
