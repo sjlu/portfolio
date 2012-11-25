fec98a8dd38b4bc6b3a2865699b92b455f0dc34a
Steven Lu <tacticalazn@gmail.com>
2012-11-25 09:26:40 -0500

    Once again, fixing it.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,12 +8,13 @@
    <meta name="keywords" content="<?= $meta['meta_keywords'] ?>" />
 
    <title><? if(!empty($title)) echo $title.' - '; ?><?= $meta['title'] ?></title>
-t
-   <link href="<?= base_url('resources/css/style.css') ?>" rel="stylesheet" type="text/css" charset="utf-8" />
-   <link href="<?= base_url('resources/css/jquery.fancybox-1.3.4.css') ?>" rel="stylesheet" type="text/css" charset="utf-8" />
 
    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
    <script type="text/javascript" src="<?= base_url('resources/js/jquery.bxslider.min.js') ?>"></script>
    <script type="text/javascript" src="<?= base_url('resources/js/jquery.fancybox-1.3.4.pack.js') ?>"></script>
+
+   <link href="<?= base_url('resources/css/style.css') ?>" rel="stylesheet" type="text/css" charset="utf-8" />
+   <link href="<?= base_url('resources/css/jquery.fancybox-1.3.4.css') ?>" rel="stylesheet" type="text/css" charset="utf-8" />
+
 </head>
 <body>
